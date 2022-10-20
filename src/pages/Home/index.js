2149fcfa19d082f203d3b7e75f850542485f0fb0
MR.HACO <jasonytonlinecomeandsee@gmail.com>
2022-10-21 00:04:44 +0800

    Update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -20,46 +20,99 @@ export default function Home() {
 
     return (
         <div className="select-none">
-
-            <div class="bg-indigo-600">
-                <div class="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
-                    <div class="flex flex-wrap items-center justify-between">
-                        <div class="flex w-0 flex-1 items-center">
-                            <span class="flex rounded-lg bg-indigo-800 p-2">
-                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
+            <div className={"bg-indigo-600" + (show ? "" : " hidden")}>
+                <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+                    <div className="flex flex-wrap items-center justify-between">
+                        <div className="flex w-0 flex-1 items-center">
+                            <span className="flex rounded-lg bg-indigo-800 p-2">
+                                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
                                 </svg>
                             </span>
-                            <p class="ml-3 truncate font-medium text-white">
-                                <span class="md:hidden">本站已移至 kd1.haco.tw，將於10/31移除！</span>
-                                <span class="hidden md:inline text-xl">本站已移至 kd1.haco.tw，預計於民國111年10月31日移除本站！</span>
+                            <p className="ml-3 truncate font-medium text-white">
+                                <span className="md:hidden">本站已移至 kd1.haco.tw，將於10/31移除！</span>
+                                <span className="hidden md:inline text-xl">本站已移至 kd1.haco.tw，預計於民國111年10月31日移除本站！</span>
                             </p>
                         </div>
-                        <div class="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
-                            <a href="https://kd1.haco.tw/" class="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50">移至新站</a>
+                        <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
+                            <a href="https://kd1.haco.tw/" className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50">移至新站</a>
                         </div>
-                        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-                            <button type="button" class="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
-                                <span class="sr-only">關閉</span>
-                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+                            <button type="button" className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2" onClick={() => setShow(!show)}>
+                                <span className="sr-only">關閉</span>
+                                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
+            {/* <br />
+            <br />
+            <h1 className='text-4xl'>最新公告</h1>
+            <br />
+            <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left text-white">
+                    <thead className="text-xs uppercase bg-indigo-800 text-white">
+                        <tr>
+                            <th scope="col" className="py-3 px-6 text left">
+                                ID
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                標題
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                內容
+                            </th>
+                            <th scope="col" className="py-3 px-6 text-right">
+                                <span className="sr-only">分享</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-indigo-500 hover:bg-indigo-400">
+                            <th scope="row" className="py-4 px-6 text left">
+                                1
+                            </th>
+                            <td className="py-4 px-6">
+                                TEST
+                            </td>
+                            <td className="py-4 px-6">
+                                TEST內容TEST內容TEST內容TEST內容TEST內容
+                            </td>
+                            <td className="py-4 px-6 text-right">
+                                <a href="#" className="font-medium text-white hover:underline">分享</a>
+                            </td>
+                        </tr>
+                        <tr className="bg-indigo-500 hover:bg-indigo-400">
+                            <th scope="row" className="py-4 px-6 text left">
+                                1
+                            </th>
+                            <td className="py-4 px-6">
+                                TEST標題
+                            </td>
+                            <td className="py-4 px-6">
+                                TEST內容TEST內容TEST內容TEST內容TEST內容
+                            </td>
+                            <td className="py-4 px-6 text-right">
+                                <a href="#" className="font-medium text-white hover:underline">分享</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br />
+            <br /> */}
+            <br />
+            <h1 className='text-4xl'>常用連結</h1>
+            <br />
+            <a href='/course'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[15%] w-[40%] h-20">課表</button></a>
+            <a href='/people'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[15%] w-[40%] h-20">人物</button></a>
+            <a href='/note'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[15%] w-[40%] h-20">筆記</button></a>
+            <a href='/test'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[15%] w-[40%] h-20">測試</button></a>
             <br />
             <br />
-            {/* <h1 className='text-4xl'>最新公告</h1> */}
-            <h1 className='text-4xl'>很明顯這是個首頁</h1>
-            <br />
-            <br />
-            <a href='/course'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看課表</button></a>
-            <a href='/people'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看人物</button></a>
-            <br />
-            <a href='/note'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看筆記</button></a>
-            <a href='/test'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看測試</button></a>
         </div>
     )
 }
--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -1,13 +1,69 @@
-import Badge from 'react-bootstrap/Badge';
-
+import axios from "axios";
+import React from "react";
+import { useLocation } from "react-router-dom"
 
 export default function Home() {
+    const location = useLocation()
+    const params = new URLSearchParams(location.search)
+    const [get, setGet] = React.useState({ "testID": "No Data", "testTitle": "No Data", "testContent": "No Data" });
+    const db_id = params.get("id")
+    React.useEffect(() => {
+        axios
+            .get('http://localhost:8080/test?id=' + db_id)
+            .then((response) => {
+                if (!response.data.message && db_id) {
+                    setGet(response.data);
+                }
+            });
+    }, [db_id]);
+
     return (
-        <div>
+        <div className='text-5xl'>
             <br />
-            <h1>This is a test page.</h1>
+            <p className='text-3xl'>DB Test Area</p>
             <br />
-            <h1><Badge bg="danger">New</Badge></h1>
+            <div className="flex w-[70%] ml-[15%]">
+                <div className="flex-1">
+                    <p className='text-2xl'>Test ID</p>
+                    <p className='text-xl'>{get.testID.toString()}</p>
+                </div>
+                <div className="flex-1">
+                    <p className='text-2xl'>Test Title</p>
+                    <p className='text-xl'>{get.testTitle.toString()}</p>
+                </div>
+                <div className="flex-1">
+                    <p className='text-2xl'>Test Content</p>
+                    <p className='text-xl'>{get.testContent.toString()}</p>
+                </div>
+            </div>
+            <br />
+            <div className="flex w-[30%] ml-[35%]">
+                {/* <div className="flex-1">
+                    <a href="/test?id=1"><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xl">增加Param</button></a>
+                </div> */}
+                <div className="flex-1">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xl" onClick={Submit}>測試</button>
+                </div>
+            </div>
+
         </div>
     )
+
+    function Submit() {
+        if (!db_id) {
+            axios
+                .post('http://localhost:8080/test?id=')
+                .then((response) => {
+                    console.log(response.data)
+                    alert(response.data.message)
+                });
+        } else {
+            axios
+                .post('http://localhost:8080/test?id=' + db_id)
+                .then((response) => {
+                    console.log(response.data)
+                    alert(response.data.message)
+                });
+        }
+    }
 }
