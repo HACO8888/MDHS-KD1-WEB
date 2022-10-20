f0897522782a3869ae1fc10babf9ec54f7a2f7a3
MR.HACO <jasonytonlinecomeandsee@gmail.com>
2022-10-20 23:36:16 +0800

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -20,28 +20,46 @@ export default function Home() {
 
     return (
         <div className="select-none">
-            <br />
-            <div id="toast-warning" className={"m-[auto] flex items-center p-4 w-full max-w-[25rem] text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" + (show ? "" : " hidden")} role="alert">
-                <div className="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
-                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path></svg>
-                    <span className="sr-only">警告標誌</span>
+
+            <div class="bg-indigo-600">
+                <div class="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+                    <div class="flex flex-wrap items-center justify-between">
+                        <div class="flex w-0 flex-1 items-center">
+                            <span class="flex rounded-lg bg-indigo-800 p-2">
+                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
+                                </svg>
+                            </span>
+                            <p class="ml-3 truncate font-medium text-white">
+                                <span class="md:hidden">本站已移至 kd1.haco.tw，將於10/31移除！</span>
+                                <span class="hidden md:inline text-xl">本站已移至 kd1.haco.tw，預計於民國111年10月31日移除本站！</span>
+                            </p>
+                        </div>
+                        <div class="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
+                            <a href="https://kd1.haco.tw/" class="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50">移至新站</a>
+                        </div>
+                        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+                            <button type="button" class="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                                <span class="sr-only">關閉</span>
+                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className="ml-3 text-sm font-normal">本網站已移至 kd1.haco.tw&emsp;&emsp;&emsp;&emsp;<a className="text-blue-200" href='https://kd1.haco.tw'>點擊前往</a></div>
-                <button type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-warning" aria-label="Close" onClick={() => setShow(!show)}>
-                    <span className="sr-only">關閉</span>
-                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                </button>
             </div>
             <br />
             <br />
+            {/* <h1 className='text-4xl'>最新公告</h1> */}
             <h1 className='text-4xl'>很明顯這是個首頁</h1>
             <br />
             <br />
-            <a href='/course'><button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看課表</button></a>
-            <a href='/people'><button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看人物</button></a>
+            <a href='/course'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看課表</button></a>
+            <a href='/people'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看人物</button></a>
             <br />
-            <a href='/note'><button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看筆記</button></a>
-            <a href='/test'><button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看測試</button></a>
+            <a href='/note'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看筆記</button></a>
+            <a href='/test'><button type="button" className="text-white bg-gradient-to-r bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2 md:w-[25%] w-[80%] h-20">點我查看測試</button></a>
         </div>
     )
 }
