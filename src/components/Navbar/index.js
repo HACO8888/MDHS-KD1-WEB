3b7548a7dab2418a0415a860a1937f6ec0ab241b
MR.HACO <jasonytonlinecomeandsee@gmail.com>
2022-10-25 01:03:17 +0800

    Update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,6 @@ function NavBar() {
     const cookies = new Cookies();
     const [navbarOpen, setNavbarOpen] = React.useState(false);
     const [loginMenu, setLoginMenu] = React.useState(false);
-    const [discord, setDiscord] = React.useState('nope');
 
     return (
         <>
@@ -106,8 +105,8 @@ function NavBar() {
                             </svg>
                         </button>
 
-                        <div className="">
-                            <button className={"text-white bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-sm px-5 py-3 text-center flex" + (!cookies.get('discord') ? "" : " hidden")} type="button" onClick={(e) => window.open("https://discord.com/api/oauth2/authorize?client_id=1033339136028979330&redirect_uri=" + (window.location.protocol) + "//" + (window.location.hostname + (window.location.port ? ':' + window.location.port : '')) + "/login&response_type=token&scope=identify", "_parent")}>
+                        <div>
+                            {/* <button className={"text-white bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-sm px-5 py-3 text-center flex" + (!cookies.get('discord') ? "" : " hidden")} type="button" onClick={(e) => window.open("https://discord.com/api/oauth2/authorize?client_id=1033339136028979330&redirect_uri=" + (window.location.protocol) + "//" + (window.location.hostname + (window.location.port ? ':' + window.location.port : '')) + "/login&response_type=token&scope=identify", "_parent")}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-discord flex-1 align-center" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                     <circle cx="9" cy="12" r="1"></circle>
@@ -120,18 +119,46 @@ function NavBar() {
                                 <p className="flex-2 ml-[5px] align-center">登入</p>
                             </button>
                             <button className={"items-center text-white bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-sm px-3 py-3 text-center flex w-[auto]" + (cookies.get('discord') ? "" : " hidden")} type="button" onClick={() => setLoginMenu(!loginMenu)}>
-                                <img className="align-center rounded-full flex-none w-[25px] h-[25px] m-[auto] mr-[3px]" src={(!cookies.get('discord') ? "" : "https://cdn.discordapp.com/avatars/" + (!cookies.get('discord') ? "" : cookies.get('discord')[2]) + "/" + (!cookies.get('discord') ? "" : cookies.get('discord')[3]) + ".png")} />
+                                <img className="align-center rounded-full flex-none w-[25px] h-[25px] m-[auto] mr-[3px]" src={(!cookies.get('discord') ? "" : "https://cdn.discordapp.com/avatars/" + (!cookies.get('discord') ? "" : cookies.get('discord')[2]) + "/" + (!cookies.get('discord') ? "" : cookies.get('discord')[3]) + ".png")} alt="user-img"/>
                                 <p className="block truncate align-center flex-initial max-w-[60px]">{!cookies.get('discord') ? "" : cookies.get('discord')[0]}</p>
                                 <p className="block align-center flex-1">#{!cookies.get('discord') ? "" : cookies.get('discord')[1]}</p>
                                 <svg className="ml-2 w-4 h-4 hidden md:inline" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg>
                             </button>
-                            <div className={"m-[auto] absolute mt-[5px] z-10 block w-[30%] md:w-[10%] bg-indigo-500 rounded-lg" + (loginMenu ? "" : " hidden")}>
+                            <div className={"m-[auto] absolute mt-[5px] z-10 block w-[auto] md:w-[auto] bg-indigo-500 rounded-lg" + (loginMenu ? "" : " hidden")}>
                                 <ul className="py-1 text-smtext-gray-200 ml-[5px] mr-[5px]">
                                     <li>
                                         <a href="#/admin" className="block py-2 px-4 w-[100%] mt-[5px] rounded-lg hover:bg-indigo-500 md:hover:bg-indigo-400 text-white">管理後臺</a>
                                     </li>
                                     <li>
                                         <a href="/logout" className="block py-2 px-4 w-[100%] mb-[5px] rounded-lg hover:bg-indigo-500 md:hover:bg-indigo-400 text-white">登出帳號</a>
+                                    </li>
+                                </ul>
+                            </div> */}
+                            <button className={"text-white bg-indigo-600 hover:bg-indigo-700 font-medium rounded-full text-sm px-5 py-3 text-center flex" + (!cookies.get('discord') ? "" : " hidden")} type="button" onClick={(e) => window.open("https://discord.com/api/oauth2/authorize?client_id=1033339136028979330&redirect_uri=" + (window.location.protocol) + "//" + (window.location.hostname + (window.location.port ? ':' + window.location.port : '')) + "/login&response_type=token&scope=identify", "_parent")}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-discord flex-1 align-center" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <circle cx="9" cy="12" r="1"></circle>
+                                    <circle cx="15" cy="12" r="1"></circle>
+                                    <path d="M7.5 7.5c3.5 -1 5.5 -1 9 0"></path>
+                                    <path d="M7 16.5c3.5 1 6.5 1 10 0"></path>
+                                    <path d="M15.5 17c0 1 1.5 3 2 3c1.5 0 2.833 -1.667 3.5 -3c.667 -1.667 .5 -5.833 -1.5 -11.5c-1.457 -1.015 -3 -1.34 -4.5 -1.5l-1 2.5"></path>
+                                    <path d="M8.5 17c0 1 -1.356 3 -1.832 3c-1.429 0 -2.698 -1.667 -3.333 -3c-.635 -1.667 -.476 -5.833 1.428 -11.5c1.388 -1.015 2.782 -1.34 4.237 -1.5l1 2.5"></path>
+                                </svg>
+                                <p className="flex-2 ml-[5px] align-center">登入</p>
+                            </button>
+                            <button className={"items-center text-white bg-indigo-600 hover:bg-indigo-700 font-medium rounded-full text-xl px-3 py-3 text-center flex w-[auto]" + (cookies.get('discord') ? "" : " hidden")} type="button" onClick={() => setLoginMenu(!loginMenu)}>
+                                <img className="align-center rounded-full flex-none w-[25px] h-[25px] m-[auto] mr-[3px]" src={(!cookies.get('discord') ? "" : "https://cdn.discordapp.com/avatars/" + (!cookies.get('discord') ? "" : cookies.get('discord')[2]) + "/" + (!cookies.get('discord') ? "" : cookies.get('discord')[3]) + ".webp")} alt="user-img" />
+                                {/* <p className="block truncate align-center flex-initial max-w-[60px]">{!cookies.get('discord') ? "" : cookies.get('discord')[0]}</p>
+                                <p className="block align-center flex-1">#{!cookies.get('discord') ? "" : cookies.get('discord')[1]}</p> */}
+                                <svg className="ml-2 w-5 h-5 flex-1" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg>
+                            </button>
+                            <div className={"w-[75px] m-[auto] absolute mt-[5px] mr-[10px] z-10 block bg-indigo-500 rounded-lg" + (loginMenu ? "" : " hidden")}>
+                                <ul className="py-1 text-smtext-gray-200 ml-[5px] mr-[5px]">
+                                    <li>
+                                        <a href="#/admin" className="block py-2 px-4 w-[100%] mt-[5px] rounded-lg hover:bg-indigo-500 md:hover:bg-indigo-400 text-white">管理</a>
+                                    </li>
+                                    <li>
+                                        <a href="/logout" className="block py-2 px-4 w-[100%] mb-[5px] rounded-lg hover:bg-indigo-500 md:hover:bg-indigo-400 text-white">登出</a>
                                     </li>
                                 </ul>
                             </div>
--- src/pages/Note/index.js
+++ src/pages/Note/index.js
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 
 export default function Note() {
     const [publicShow, setPublicShow] = useState(false);
-    const [privateShow, setPrivateShow] = useState(false);
+    // const [privateShow, setPrivateShow] = useState(false);
     const [notes, setNotes] = React.useState([{ "ID": "No Data", "Title": "No Data", "Content": "No Data", "Type": "No Data" }]);
 
     React.useEffect(() => {
@@ -20,95 +20,6 @@ export default function Note() {
 
     const Html = [];
     Html.push(
-        <div className='flex'>
-            {/* <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mr-5">
-                <a href="#">
-                    <img class="rounded-t-lg" src="/pictures/920x613.jpg" alt="" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </a>
-                </div>
-            </div>
-            <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <img class="rounded-t-lg" src="/pictures/920x613.jpg" alt="" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </a>
-                </div>
-            </div>
-            <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ml-5">
-                <a href="#">
-                    <img class="rounded-t-lg" src="/pictures/920x613.jpg" alt="" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </a>
-                </div> 
-            </div>*/}
-            {/* <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1">
-                <img className="w-full" src="/pictures/800x487.jpg" alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-            </div>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1">
-                <img className="w-full" src="/pictures/800x487.jpg" alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-            </div>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1">
-                <img className="w-full" src="/pictures/800x487.jpg" alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-            </div> */}
-        </div>
     );
 
     return (
@@ -127,7 +38,7 @@ export default function Note() {
             { /* Cards Here Start*/}
             {Html}
             { /* Cards Here End*/}
-            <div tabIndex="-1" aria-hidden="true" className={"modal fixed w-full h-full top-0 left-0 flex items-center justify-center" + (publicShow ? "" : " hidden")}>
+            <div tabIndex="-1" aria-hidden="true" className={"modal fixed w-full h-full top-0 left-0 flex items-center justify-center z-30" + (publicShow ? "" : " hidden")}>
                 <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
                 <div className="modal-container w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
                     <div className="relative bg-slate-900 rounded-lg shadow">
@@ -170,60 +81,6 @@ export default function Note() {
                     </div>
                 </div>
             </div>
-            {/* <Modal
-                show={show}
-                onHide={handleClose}
-                backdrop="static"
-                keyboard={false}
-                className="modal-control"
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Form className='note-form' onSubmit={HandleNoteSubmit}>
-                    <Modal.Header className="modal-dark noSelect">
-                        <Modal.Title id="contained-modal-title-vcenter" className="modal-title">
-                            感謝您新增本班共享筆記內容
-                        </Modal.Title>
-                        <CloseButton variant="white" aria-label="Hide" onClick={handleClose} />
-                    </Modal.Header>
-                    <Modal.Body className="modal-dark">
-                        <Alert key="warning" variant="warning">
-                            請按照表格填寫所有資訊以便後續資料處裡！
-                        </Alert>
-
-                        <p className="modal-body-title noSelect">請選擇記事類別:</p>
-                        <Form.Select className="modal-body-select" disabled>
-                            <option>此功能尚未開放</option>
-                            <option>廢文</option>
-                            <option>筆記</option>
-                            <option>迷因</option>
-                        </Form.Select>
-                        <p className="modal-body-title paddingtop noSelect">請輸入記事標題:</p>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="NoteSubmitForm.Title"
-                        >
-                            <Form.Control className="modal-body-input-short" as="textarea" rows={3} maxLength="20" />
-                        </Form.Group>
-                        <p className="modal-body-title noSelect">請輸入記事內容:</p>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="NoteSubmitForm.Content"
-                        >
-                            <Form.Control className="modal-body-input-long" as="textarea" rows={3} maxLength="1000" />
-                        </Form.Group>
-
-
-                        <br />
-                    </Modal.Body>
-                    <Modal.Footer className="modal-dark noSelect">
-                        <Button variant="false" className="cancle-button" aria-label="Hide" onClick={handleClose}><p className="modal-cancle-button-text">取消</p></Button>
-                        <Button variant="false" className="modal-button" type="submit"><p className="modal-button-text">送出</p></Button>
-                    </Modal.Footer>
-                </Form>
-            </Modal> */}
-
         </div >
     )
 }
