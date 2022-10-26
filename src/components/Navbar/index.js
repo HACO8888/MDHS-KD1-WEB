b69372460b3bed6f8deb04a52d15def91aabc63d
MR.HACO <jasonytonlinecomeandsee@gmail.com>
2022-10-27 00:01:40 +0800

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -76,14 +76,12 @@ function NavBar() {
                     <div className="flex gap-3">
                         {/* Notify Button */}
                         <button className="text-white bg-cyan-600 hover:bg-cyan-600 md:hover:bg-cyan-700 font-medium rounded-full text-sm px-3 py-3 text-center flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bell" width="24"
-                                height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none"
-                                strokeLinecap="round" strokeLinejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-palette" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path
-                                    d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6">
-                                </path>
-                                <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
+                                <path d="M12 21a9 9 0 1 1 0 -18a9 8 0 0 1 9 8a4.5 4 0 0 1 -4.5 4h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25"></path>
+                                <circle cx="7.5" cy="10.5" r=".5" fill="currentColor"></circle>
+                                <circle cx="12" cy="7.5" r=".5" fill="currentColor"></circle>
+                                <circle cx="16.5" cy="10.5" r=".5" fill="currentColor"></circle>
                             </svg>
                         </button>
                         <button className="text-white bg-cyan-600 font-medium rounded-full text-sm px-3 py-3 text-center flex md:hidden" onClick={() => setNavbarOpen(!navbarOpen)}>
