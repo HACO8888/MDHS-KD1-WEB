62672a183f7e40e7b9acdc1b990460e4d285f67a
MR.HACO <jasonytonlinecomeandsee@gmail.com>
2022-10-27 00:08:02 +0800

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,6 +6,7 @@ function NavBar() {
     const cookies = new Cookies();
     const [navbarOpen, setNavbarOpen] = React.useState(false);
     const [loginMenu, setLoginMenu] = React.useState(false);
+    const [colorMenu, setColorMenu] = React.useState(false);
 
     return (
         <>
@@ -74,16 +75,30 @@ function NavBar() {
                         </a>
                     </div>
                     <div className="flex gap-3">
-                        {/* Notify Button */}
-                        <button className="text-white bg-cyan-600 hover:bg-cyan-600 md:hover:bg-cyan-700 font-medium rounded-full text-sm px-3 py-3 text-center flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-palette" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M12 21a9 9 0 1 1 0 -18a9 8 0 0 1 9 8a4.5 4 0 0 1 -4.5 4h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25"></path>
-                                <circle cx="7.5" cy="10.5" r=".5" fill="currentColor"></circle>
-                                <circle cx="12" cy="7.5" r=".5" fill="currentColor"></circle>
-                                <circle cx="16.5" cy="10.5" r=".5" fill="currentColor"></circle>
-                            </svg>
-                        </button>
+                        {/* Notify Button Start */}
+                        <div>
+                            <button className="text-white bg-cyan-600 hover:bg-cyan-600 md:hover:bg-cyan-700 font-medium rounded-full text-sm px-3 py-3 text-center flex"  onClick={() => setColorMenu(!colorMenu)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-palette" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M12 21a9 9 0 1 1 0 -18a9 8 0 0 1 9 8a4.5 4 0 0 1 -4.5 4h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25"></path>
+                                    <circle cx="7.5" cy="10.5" r=".5" fill="currentColor"></circle>
+                                    <circle cx="12" cy="7.5" r=".5" fill="currentColor"></circle>
+                                    <circle cx="16.5" cy="10.5" r=".5" fill="currentColor"></circle>
+                                </svg>
+                            </button>
+                            {/* <div className={"w-[100px] m-[auto] absolute mt-[5px] mr-[20px] z-10 block bg-cyan-500 rounded-lg" + (colorMenu ? "" : " hidden")}>
+                                <ul className="py-1 text-smtext-gray-200 ml-[5px] mr-[5px]">
+                                    <li>
+                                        <a href="#/admin" className="block py-2 px-4 w-[100%] mt-[5px] rounded-lg hover:bg-cyan-500 md:hover:bg-cyan-400 text-white">管理</a>
+                                        
+                                    </li>
+                                    <li>
+                                        <a href="/logout" className="block py-2 px-4 w-[100%] mb-[5px] rounded-lg hover:bg-cyan-500 md:hover:bg-cyan-400 text-white">登出</a>
+                                    </li>
+                                </ul>
+                            </div> */}
+                        </div>
+                        {/* Notify Button End */}
                         <button className="text-white bg-cyan-600 font-medium rounded-full text-sm px-3 py-3 text-center flex md:hidden" onClick={() => setNavbarOpen(!navbarOpen)}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="24"
                                 height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none"
