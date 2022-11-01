604ed7621a9bb468ab15f23752491bf84b9d1cdb
MR.HACO <jasonytonlinecomeandsee@gmail.com>
2022-11-02 00:55:28 +0800

    Update

--- src/pages/Announcements/ID/index.js
+++ src/pages/Announcements/ID/index.js
@@ -27,19 +27,34 @@ export default function Home() {
             <br />
             <br />
             <br />
-            <p className='text-3xl'>公告編號</p>
+            <br />
+            <p className='text-3xl text-cyan-300'>公告編號</p>
             <br />
             <p className='text-2xl'>{ann.ID}</p>
             <br />
             <br />
-            <p className='text-3xl'>公告標題</p>
+            <br />
+            <p className='text-3xl text-cyan-300'>公告標題</p>
             <br />
             <p className='text-2xl'>{ann.Title}</p>
             <br />
             <br />
-            <p className='text-3xl'>公告內容</p>
             <br />
-            <p className='text-2xl w-[80%] ml-[10%]'>{ann.Content}</p>
+            <p className='text-3xl text-cyan-300'>公告內容</p>
+            <br />
+            <p className='text-2xl w-[60%] ml-[20%]'>{ann.Content}</p>
+            <br />
+            <br />
+            <br />
+            <p className='text-3xl text-cyan-300'>播放公告</p>
+            <br />
+            <div className="flex flex-col my-auto items-center bgimg bg-cover">
+                <audio className='text-2xl items-center align-center' controls>
+                    <source src="/voices/吳孟熹做愛.mp3" type="audio/mpeg" />
+                    您的瀏覽器不支援撥放聲音!
+                </audio>
+            </div>
+            <br />
             <br />
             <br />
         </div>
